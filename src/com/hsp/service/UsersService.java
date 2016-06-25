71076744782673fcfc3ea175c833887a2a1691cd
wsfy <wsfy_gzh@163.com>
2016-06-25 22:30:57 +0800

    看韩顺平视频写代码，逐步完善控制器。

--- src/com/hsp/service/UsersService.java
+++ src/com/hsp/service/UsersService.java
@@ -4,13 +4,87 @@ import com.hsp.domain.User;
 import com.hsp.util.SqlHelper;
 
 import java.sql.*;
+import java.util.ArrayList;
 public class UsersService {
 
+	
+	// 获取pageCount
+	public int getPageCount(int pageSize) {
+		
+		
+		String sql = "select count(*) from users";
+		int rowCount = 0;
+		ResultSet rs = SqlHelper.executeQuery(sql, null);
+		try {
+			rs.next();
+			rowCount = rs.getInt(1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {// 关闭资源
+			SqlHelper.close(rs, SqlHelper.getPs(), SqlHelper.getCt());// 反向引用
+		}
+		
+		
+		return (rowCount - 1) / pageSize + 1;
+		
+	}
+	
+	// 按照分页来获取用户
+	// 公司 把 ResultSet -> User对象 -> ArrayList()（即顺序表，在Java中理解为集合的一种）
+	public ArrayList getUsersByPage(int pageNow, int pageSize) {
+		
+		
+		ArrayList al = new ArrayList<User>();
+		
+		// 查询sql
+		String sql = "select * from (select t.*, rownum rn from " +
+				"(select * from users order by id) t where rownum<=" + pageSize * pageNow + ") where rn>=" + (pageSize * (pageNow - 1) + 1); 
+		
+		ResultSet rs = SqlHelper.executeQuery(sql, null);
+		// 二次封装 把 ResultSet -> User对象 -> ArrayList()（即顺序表，在Java中理解为集合的一种）
+	
+		try {
+			while (rs.next()) {
+				User u = new User();
+				u.setId(rs.getInt(1));
+				u.setName(rs.getString(2));
+				u.setEmail(rs.getString(3));
+				u.setGrade(rs.getInt(4));
+				// 千万不要王佳 u->ArrayList，即把对象封装到集合（顺序表）中。
+				al.add(u);// 把User类的对象添加到集合对象中
+				
+			}	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {// 关闭资源
+			SqlHelper.close(rs, SqlHelper.getPs(), SqlHelper.getCt());// 反向引用
+		}
+			
+			
+		
+		return al;
+	}
 	
 	
 	// 添加用户
 	// 修改用户
 	// 删除用户
+	public boolean delUser(String id) {
+		boolean b = true;
+		String sql = "delete from users where id=?";
+		String parameters[] = {id};
+		try {
+			SqlHelper.executeQuery(sql, parameters);
+		} catch (Exception e) {
+			b = false;
+			// TODO Auto-generated catch block
+		}
+		
+		return b;
+		
+	}
 	
 	// 写一个验证用户是否合法的函数
 	public boolean checkUser(User user) {
--- src/com/hsp/view/ManageUsers.java
+++ src/com/hsp/view/ManageUsers.java
@@ -7,11 +7,15 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.hsp.domain.User;
+import com.hsp.service.UsersService;
 
 public class ManageUsers extends HttpServlet {
 
@@ -21,21 +25,20 @@ public class ManageUsers extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		out.println("<script type='text/javascript' language='javascript'>");
-		out.println("function gotoPageNow() { " +
-				"var pageNow=document.getElementById('pageNow'); " +
-				"window.open('/UsersManager2/ManageUsers?pageNow=' + pageNow.value,'_self');}");
+		out.println("function gotoPageNow() {" +
+				"var pageNow = document.getElementById('pageNow');" +
+				"window.open('/UsersManager2/ManageUsers?pageNow='+pageNow.value,'_self');}" +
+				"function confirmOper() {return window.confirm('真的要删除该用户吗？');}");
+		
 		out.println("</script>"); 
 		out.println("<img src='imgs/1.gif'/>欢迎**登录 <a href='/UsersManager2/LoginServlet'>返回主界面</a> <a href='/UsersManager2/LoginServlet'>安全退出</a><hr/>");
 		out.println("<h1>管理用户</h1>");
-		// 从数据库中取出用户数据，并显示
-		// 到数据库中去验证
-		Connection ct = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+	
 		
 		// 定义分页需要的变量
 		
 		int pageNow = 1;// 当前页
+		int pageCount = 0;
 		
 		// 接受用户的pageNow
 		String sPageNow = request.getParameter("pageNow");
@@ -44,47 +47,30 @@ public class ManageUsers extends HttpServlet {
 			pageNow = Integer.parseInt(sPageNow);
 		}
 		
-		
 		int pageSize = 3;// 指定每页显示3条记录
-		int pageCount = 1;// 总共有多少页。该值是计算出来的。
-		int rowCount = 1;// 表示共有多少记录，该值是查询数据库得到
 		
-		
-		try {
+		try {// 该界面通过调用Service类中的方法显示数据
 			
-			// 1、加载驱动
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			// 2、得到连接
-			ct = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "scott", "tiger");
+			UsersService usersService = new UsersService();
 			
-			// 算出共有多少页
-			// 1、查询rowCount
-			ps = ct.prepareStatement("select count(*) from users");
-			rs = ps.executeQuery();
-			rs.next();// 把游标下移
-			rowCount = rs.getInt(1);
+			pageCount = usersService.getPageCount(pageSize);
 			
-			pageCount = rowCount % pageSize == 0 ? rowCount / pageSize : rowCount / pageSize + 1;
 			
-			// 3、创建PreparedStatement对象（创建预处理语句对象）
-			// 请大家思考，如果给出的条件 pageNow=3 pageSize=2
-			ps = ct.prepareStatement("select * from (select t.*, rownum rn from " +
-					"(select * from users order by id) t where rownum<=" + pageSize * pageNow + ") where rn>=" + (pageSize * (pageNow - 1) + 1));
-			String sql = "select * from (select t.*, rownum rn from " +
-					"(select * from users order by id) t where rownum<=" + pageSize * pageNow + ") where rn>=" + (pageSize * (pageNow - 1) + 1);
-			System.out.println("sql=" + sql);
-			// 4、执行操作
-			rs = ps.executeQuery();
-			out.println("<table border=1px bordercolor=green cellspacing=0 width=500px>");
-			out.println("<tr><th>id</th><th>用户名</th><th>email</th><th>级别</th></tr>");
-			// 循环显示所有用户信息
-			while (rs.next()) {
+			
 				
-				out.println("<tr><td>" + rs.getInt(1) + 
-						"</td><td>" + rs.getString(2) + 
-						"</td><td>" + rs.getString(3) + 
-						"</td><td>" + rs.getInt(4) + 
-						"</td></tr>");
+			
+			
+			ArrayList<User> al = usersService.getUsersByPage(pageNow, pageSize);
+			
+			out.println("<table border=1px bordercolor=green cellspacing=0 width=500px>");
+			out.println("<tr><th>id</th><th>用户名</th><th>email</th><th>级别</th><th>删除用户</th><th>修改用户</th></tr>");
+			// 循环显示所有用户信息
+			for (User u : al) {
+				out.println("<tr><td>" + u.getId() + 
+						"</td><td>" + u.getName() + 
+						"</td><td>" + u.getEmail() + 
+						"</td><td>" + u.getGrade() + 
+						"</td><td><a onClick='return confirmOper();' href='/UsersManager2/DelClServlet?type=del&id=" + u.getId() + "'>删除用户</a></td><td><a href='#'>修改用户</a></td></tr>");
 			}
 			out.println("</table><br/>");
 			
@@ -109,36 +95,8 @@ public class ManageUsers extends HttpServlet {
 			out.println("跳转到<input type='text' id='pageNow' name='pageNow'/> <input type='button' onClick='gotoPageNow()' value='跳转'/>");
 		} catch (Exception e) {
 			// TODO: handle exception
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				rs = null;
-			}
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				ps = null;
-			}	
-			if (ct != null) {
-				try {
-					ct.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				ct = null;
-			}
+		} 
 			
-		}
 		out.println("<hr/><img src='imgs/mylogo.gif'/>");
 
 	}
